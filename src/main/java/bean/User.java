8e4de5f3b9ec02e5cbe5fd56eeec70af2c57f68f
ayoubfarid <ayoubfarid80@gmail.com>
2023-04-10 22:53:26 +0000

    improve login functionnality

--- src/main/java/bean/User.java
+++ src/main/java/bean/User.java
@@ -3,7 +3,14 @@ package bean;
 import dao.Compte;
 import dao.DAOCompte;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @ManagedBean
 public class User {
@@ -11,6 +18,45 @@ public class User {
     private String username;
 
     private String password;
+    private int genre;
+    private String fullname;
+    private String pays;
+    private String adresse;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    private int age;
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
 
     public String getFullname() {
         return fullname;
@@ -20,12 +66,14 @@ public class User {
         this.fullname = fullName;
     }
 
-    private String fullname;
+
     public User() {
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -33,34 +81,78 @@ public class User {
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
 
-    public String login(){
-        DAOCompte dao=new DAOCompte();
-        Compte compte=dao.getCompte(username);
-        if(compte!=null){
-            if(compte.getPassword().equals(password)){
-                fullname=compte.getFullname();
+    public String login() {
+        DAOCompte dao = new DAOCompte();
+        Compte compte = dao.getCompte(username);
+        if (compte != null) {
+            if (compte.getPassword().equals(password)) {
+
+                fullname = compte.getFullname();
                 return "success";
+
             }
+        }
+
+        return "failure";
+    }
+
+    public String register() {
+        DAOCompte dao = new DAOCompte();
+        Compte compte = new Compte(username, password, fullname, genre, age, pays, adresse);
+        if (compte.getPassword() != null && compte.getUsername() != null) {
+            dao.create(compte);
+            fullname = compte.getFullname();
+            return "success";
+
+
         }
 
         return "login";
     }
 
-    public String register(){
-        DAOCompte dao=new DAOCompte();
-        Compte compte=new Compte(username,password,fullname);
-        if(compte.getPassword()!=null && compte.getUsername()!=null ){
-            dao.create(compte);
-            return "success";
+
+
+    public void validateAge(FacesContext context, UIComponent comp,
+                            Object value) {
+
+        System.out.println("inside validate age");
+
+        String age = (String) value;
+
+        if (!age.matches("^[0-9]{0,2}")) {
+            ((UIInput) comp).setValid(false);
+
+
+            FacesMessage message = new FacesMessage(
+                    "Age doit etre compris entre 18-99.");
+            context.addMessage(comp.getClientId(context), message);
 
         }
 
-        return "index";
+    }
+
+    public void validateDateNais(FacesContext context, UIComponent comp,
+                                 Object value) {
+
+        System.out.println("inside validate method");
+
+        String date = (String) value;
+
+        if (!date.matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\\d\\d$")) {
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage(
+                    "date incorrecte: ex: 31/12/2000");
+            context.addMessage(comp.getClientId(context), message);
+
+        }
+
     }
 
 }
--- src/main/java/dao/Compte.java
+++ src/main/java/dao/Compte.java
@@ -9,7 +9,49 @@ import javax.persistence.Table;
 public class Compte {
     @Id
     private String username;
+
     private String password;
+
+    private int genre;
+    private int age;
+    private String pays;
+    private String adresse;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    private String fullname;
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
+
 
     public String getFullname() {
         return fullname;
@@ -19,7 +61,7 @@ public class Compte {
         this.fullname = fullname;
     }
 
-    private String fullname;
+
 
 
 
@@ -32,6 +74,21 @@ public class Compte {
         this.username = username;
         this.password = password;
         this.fullname = fullname;
+    }
+    public Compte(String username, String password, String fullname,int genre) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.genre = genre;
+    }
+    public Compte(String username, String password, String fullname,int genre,int age,String pays,String adresse) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.genre = genre;
+        this.age = age;
+        this.pays = pays;
+        this.adresse = adresse;
     }
 
     public Compte() {
